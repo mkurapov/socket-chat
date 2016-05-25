9f5c7cc087a5e93cb404a02c09128ef197b1aca9
mkurapov <maxk@criticalmass.com>
2016-05-25 09:42:27 -0600

    added user create

--- index.js
+++ index.js
@@ -6,30 +6,6 @@ var io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
 
-var cookieSession = require('cookie-session');
-var cookieParser = require('cookie-parser');
-
-
-app.use(cookieParser());
-
-app.use(function (req, res, next) {
-  // check if client sent cookie
-  var cookie = req.cookies.cookieName;
-  if (cookie === undefined)
-  {
-    // no: set a new cookie
-    var randomNumber=Math.random().toString();
-    randomNumber=randomNumber.substring(2,randomNumber.length);
-    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
-  }
-  else
-  {
-    // yes, cookie was already present
-    console.log('cookie exists', cookie);
-  }
-  next(); // <-- important!
-});
 
 
 app.use(express.static(__dirname));
@@ -40,16 +16,26 @@ app.get('/', function(req, res){
 
 var currentMessages = [];
 var savedChat = [];
+var users = [];
 
 
 io.on('connection', function(socket){
 
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', {msg: msg, username: socket.username});
     currentMessages.push(msg);
     savedChat = currentMessages.toString();
     storeCookies(savedChat);
     console.log(savedChat);
+
+  });
+
+  socket.on('user create', function(user){
+    socket.username = user;
+    io.emit('user create', user);
+    users.push(user);
+    console.log(users);
+
 
   });
 });
